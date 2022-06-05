590995edefd8fc36c982e359fe5ece088f66d1e9
legend80s <liuchzong@qq.com>
2022-06-05 18:13:50 +0800

    fix: cannot resolve solid-js

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createSignal, createMemo } from 'solid-js';
+import { createSignal } from 'solid-js';
 import { createStore } from 'solid-js/store';
 import { getItem, setItem } from './helper/storage';
 
