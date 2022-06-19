2bcc9a1089214d37fe88e150a0fc6b112fd9d641
legend80s <liuchzong@qq.com>
2022-06-20 02:41:27 +0800

    feat: pwa 9

--- src/index.tsx
+++ src/index.tsx
@@ -8,10 +8,10 @@ render(() => <App />, document.getElementById("root")!);
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").then((registration) => {
+    navigator.serviceWorker.register("/sw.js", { scope: '../' }).then((registration) => {
       console.log("[PWA] Service Worker registered successfully with scope:", registration.scope);
     }).catch((error) => {
-      console.log('[PWA] Service Worker registered failed:', error);
+      console.error(error);
     });
   });
 }
